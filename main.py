2269d870f9d486fa90b293e441e92a41b330f3dc
jaylinnen <jaylinnen9@gmail.com>
2026-09-03 20:29:13 -0400

    Added questions/changed grading scale

--- main.py
+++ main.py
@@ -76,24 +76,71 @@ else:
    print("Invalid input")
    exit()
 
+answer4 = input("""4. What is malware? \n
+A. Software designed to damage, disrupt, or steal information \n
+B. A tool used to improve Wi-Fi speed \n
+C. A type of secure password \n
+D. A program used only for software updates \n
+Answer:""").strip().upper()
+
+if answer4 == 'A':
+    correct += 1
+    print("Correct")
+elif answer4 == 'B':
+    incorrect += 1
+    print("Incorrect")
+elif answer4 == 'C':
+    incorrect += 1
+    print("Incorrect")
+elif answer4 == 'D':
+    incorrect += 1
+    print("Incorrect")
+else:
+    print("Invalid input")
+    exit()  
+
+answer5 = input("""5. What is the safest thing to do when using public Wi-Fi? \n
+A. Enter sensitive information on any website \n
+B. Share passwords with other users \n
+C. Avoid accessing sensitive accounts on unsecured networks \n
+D. Turn off your device password \n
+Answer:""").strip().upper()
+
+if answer5 == 'A':
+    incorrect += 1
+    print("Incorrect")
+elif answer5 == 'B':
+    incorrect += 1
+    print("Incorrect")
+elif answer5 == 'C':
+    correct += 1
+    print("Correct")
+elif answer5 == 'D':
+    incorrect += 1
+    print("Incorrect")
+else:
+    print("Invalid input")
+    exit()
+
 
 print("\n")
 print("Score:")
 print("Correct:",correct)
 print("Incorrect:",incorrect)
 
-overall = round((correct/3)*100, 1)
+overall = round((correct / 5) * 100, 1)
 
+print(overall, "%")
 
-print(overall,"%")
-
-if correct == 1:
-    print("Grade: F")
-elif correct == 2:
-    print("Grade: D")
-elif correct == 3:
+if overall >= 90:
     print("Grade: A")
-elif correct == 0:
+elif overall >= 80:
+    print("Grade: B")
+elif overall >= 70:
+    print("Grade: C")
+elif overall >= 60:
+    print("Grade: D")
+else:
     print("Grade: F")
 
 
